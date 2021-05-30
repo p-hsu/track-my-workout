e8dcee6aaafabd80168472c985887f6d6135f208
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-05-30 12:44:48 -0500

    modified Exercise.js

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-// ref seed.js for attributes required
-type: String,
-name: String,
-duration: Number,
-distance: Number,
-weight: Number,
-reps: Number,
-sets: Number,
-
+    // ref seed.js for attributes required
+    type: String,
+    name: String,
+    duration: Number,
+    distance: Number,
+    weight: Number,
+    reps: Number,
+    sets: Number,
 });
 
 const Exercise = mongoose.model("Exercise", ExerciseSchema);

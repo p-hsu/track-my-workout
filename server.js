9c0815b5c23171e8da19d1a8a685a90159d9c0d2
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-05-30 12:43:20 -0500

    modified to use seperate controllers directory

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
-const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/track-my-workout", { useNewUrlParser: true });
+require("./controllers/htmlRoutes")(app);
+require("./controllers/apiRoutes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
